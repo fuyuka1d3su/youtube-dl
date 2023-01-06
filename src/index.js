a1e95b8590b143402a1607f5ba5a7fa41d4db56f
Nazim Baraka <2174335@bdeb.qc.ca>
2023-01-06 00:59:59 -0500

    removed squirrel events

--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@ const { app, BrowserWindow, ipcMain, dialog } = require("electron");
 const path = require("path");
 const fs = require("fs");
 const SETTO_PATH = app.getPath("userData");
+console.log(SETTO_PATH);
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
-// this should be placed at top of main.js to handle setup events quickly
-if (handleSquirrelEvent()) {
-  // squirrel event handled and app will exit in 1000ms, so don't do anything else
-  return;
-}
 
 if (!fs.existsSync(path.join(SETTO_PATH, "json/recentvids.json"))) {
   fs.mkdirSync(path.join(SETTO_PATH, "json/"));
@@ -34,14 +30,14 @@ const createWindow = () => {
       contextIsolation: false,
     },
     autoHideMenuBar: true,
-    icon: path.join(__dirname, "icon.ico"),
+    icon: path.join(__dirname, "icon.png"),
   });
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, "index.html"));
 
   // Open the DevTools.
-  //mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
@@ -77,65 +73,8 @@ ipcMain.handle("showSaveDialog", (e, options) => {
   return result;
 });
 
-// Squirrel Stuff
-function handleSquirrelEvent() {
-  if (process.argv.length === 1) {
-    return false;
-  }
+ipcMain.handle("getUserData", (e) => {
+  return SETTO_PATH;
+});
 
-  const ChildProcess = require("child_process");
-  const path = require("path");
 
-  const appFolder = path.resolve(process.execPath, "..");
-  const rootAtomFolder = path.resolve(appFolder, "..");
-  const updateDotExe = path.resolve(path.join(rootAtomFolder, "Update.exe"));
-  const exeName = path.basename(process.execPath);
-
-  const spawn = function (command, args) {
-    let spawnedProcess, error;
-
-    try {
-      spawnedProcess = ChildProcess.spawn(command, args, { detached: true });
-    } catch (error) {}
-
-    return spawnedProcess;
-  };
-
-  const spawnUpdate = function (args) {
-    return spawn(updateDotExe, args);
-  };
-
-  const squirrelEvent = process.argv[1];
-  switch (squirrelEvent) {
-    case "--squirrel-install":
-    case "--squirrel-updated":
-      // Optionally do things such as:
-      // - Add your .exe to the PATH
-      // - Write to the registry for things like file associations and
-      //   explorer context menus
-
-      // Install desktop and start menu shortcuts
-      spawnUpdate(["--createShortcut", exeName]);
-
-      setTimeout(app.quit, 1000);
-      return true;
-
-    case "--squirrel-uninstall":
-      // Undo anything you did in the --squirrel-install and
-      // --squirrel-updated handlers
-
-      // Remove desktop and start menu shortcuts
-      spawnUpdate(["--removeShortcut", exeName]);
-
-      setTimeout(app.quit, 1000);
-      return true;
-
-    case "--squirrel-obsolete":
-      // This is called on the outgoing version of your app before
-      // we update to the new version - it's the opposite of
-      // --squirrel-updated
-
-      app.quit();
-      return true;
-  }
-}
